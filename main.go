95edea001193db5084c8f95aaaa93d72226ecd0f
RaptorExTolop-School <jamesappletonret@gmail.com>
2024-10-25 20:31:22 +1030

    2: added basic file reading and splitting

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	fileContents, err := os.ReadFile("helloWorld.csv")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	splittedFile := strings.Split(string(fileContents), "\n")
+	for i := 0; i < len(splittedFile); i++ {
+		fmt.Println(splittedFile[i])
+	}
 }
