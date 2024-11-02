b36c6c465e90f41b03ac44220e772893c5d4618c
RaptorExTolop <jamesappletonret@gmail.com>
2024-11-02 12:14:27 +1030

    Dev 0.0.1: Added struct for quetion and basic adding and deleting functionaltiy

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	questions := Questions{}
+
+	var blankStringList []string
+
+	questions.add("testing 1", "this is test 1", blankStringList, blankStringList)
+	questions.add("testing 2", "this is test 2", blankStringList, blankStringList)
+	questions.add("testing 3", "this is test 3", blankStringList, blankStringList)
+
+	questions.delete(1)
+
+	fmt.Println(questions)
 }
---
+++ question.go
@@ -0,0 +1,43 @@
+package main
+
+import "errors"
+
+type Question struct {
+	name            string
+	description     string
+	possibleAnswers []string
+	correctAnswers  []string
+}
+
+type Questions []Question
+
+func (qs *Questions) add(name, description string, possibleAnswers, correctAnswers []string) {
+	question := Question{
+		name:            name,
+		description:     description,
+		possibleAnswers: possibleAnswers,
+		correctAnswers:  correctAnswers,
+	}
+
+	*qs = append(*qs, question)
+}
+
+func (questions *Questions) validateIndex(index int) error {
+	if index < 0 || index >= len(*questions) {
+		err := errors.New("invalid index")
+		return err
+	}
+	return nil
+}
+
+func (questions *Questions) delete(index int) error {
+	qs := *questions
+
+	if err := qs.validateIndex(index); err != nil {
+		return err
+	}
+
+	*questions = append(qs[:index], qs[index+1:]...)
+
+	return nil
+}
