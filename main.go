77da1193d87ea6a35914b2c4dc09ad9eb214c551
RaptorExTolop <jamesappletonret@gmail.com>
2024-11-04 07:15:41 +1030

    Dev 0.0.1: Added the ability to edit commands

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	questions := Questions{}
 
 	var blankStringList []string
 
-	questions.add("testing 1", "this is test 1", blankStringList, blankStringList)
-	questions.add("testing 2", "this is test 2", blankStringList, blankStringList)
-	questions.add("testing 3", "this is test 3", blankStringList, blankStringList)
+	questions.add("testing 1", blankStringList, blankStringList)
+	questions.add("testing 2", blankStringList, blankStringList)
+	questions.add("testing 3", blankStringList, blankStringList)
+	err := questions.editName(2, "testing 3.0")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	questions.delete(1)
+	err = questions.delete(1)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	fmt.Println(questions)
 }
--- question.go
+++ question.go
@@ -3,18 +3,18 @@ package main
 import "errors"
 
 type Question struct {
-	name            string
-	description     string
+	name string
+	//	description     string
 	possibleAnswers []string
 	correctAnswers  []string
 }
 
 type Questions []Question
 
-func (qs *Questions) add(name, description string, possibleAnswers, correctAnswers []string) {
+func (qs *Questions) add(name string, possibleAnswers, correctAnswers []string) {
 	question := Question{
-		name:            name,
-		description:     description,
+		name: name,
+		//		description:     description,
 		possibleAnswers: possibleAnswers,
 		correctAnswers:  correctAnswers,
 	}
@@ -41,3 +41,15 @@ func (questions *Questions) delete(index int) error {
 
 	return nil
 }
+
+func (questions *Questions) editName(index int, newName string) error {
+	qs := *questions
+	if err := qs.validateIndex(index); err != nil {
+		return err
+	}
+
+	qs[index].name = newName
+	return nil
+}
+
+//func (questions *Questions) editDe
